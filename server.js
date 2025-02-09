7e481d535f07e58f3b514360bba7b33cabbbc1a3
Vamsi280300 <vamsikrishnaduggana28@gmail.com>
2025-02-09 18:11:22 -0500

    Added server,Axios,and ChartJS

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 // Budget API
 
 const express = require('express');
-const cors = require('cors');
+
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use('/', express.static('public'));
 
 const budget = {
     myBudget: [
@@ -25,9 +25,14 @@ const budget = {
 };
 
 
+app.get('/hello', (req, res) => {
+    res.send("hello World");
+});
+
 app.get('/budget', (req, res) => {
     res.json(budget);
 });
+
 
 app.listen(port, () => {
     console.log(`API served at http://localhost:${port}`);
